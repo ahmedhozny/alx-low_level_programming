c0f303f555c7d34ccf3ed32f5f030caf394a58dc
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-06-15 16:30:40 -0700

    Betty style is now used

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
