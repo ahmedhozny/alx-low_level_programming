0e9371dcd6e7df1941e0ad57514bed5d90421ede
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-06-22 19:58:29 -0700

    First additional task is done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -Main block
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int num, p;
+
+	num = 612852475143;
+	for (p = 2; p <= num; p++)
+	{
+		if (num % p == 0)
+		{
+			num = num / p;
+			p--;
+		}
+	}
+	
+	printf("%ld\n", p);
+	
+	return (0);
+}
