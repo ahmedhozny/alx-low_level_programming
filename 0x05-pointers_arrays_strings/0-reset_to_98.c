5fece0aedfcb2724456a2b744e32bf56481a2412
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-06-26 16:24:52 -0700

    betty formatting

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  * reset_to_98 - Resets given variable and reset it;s value to 98
- * @n: variable to be reset 
+ * @n: variable to be reset
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
