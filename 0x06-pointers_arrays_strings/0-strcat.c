d8b5ed731e66b75f82cfe0ff2df601f19031bb12
PhotonCodes <ahmhosny2002@gmail.com>
2023-06-30 01:43:58 +0300

    First task for 0x06 is finished

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat -concatenates two given strings
+ * @dest: string to be appended
+ * @src: string that will merge with @dest
+ *
+ * Return: pointer to @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, len;
+
+	for (len = 0; dest[len] != '\0'; len++);
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[len++] = src[i];
+	dest[len] = '\0';
+
+	return (dest);
+}
