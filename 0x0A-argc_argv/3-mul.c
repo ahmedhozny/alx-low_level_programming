9035c064af84d953e3bbbc8c98710c92b7456e04
PhotonCodes <ahmhosny2002@gmail.com>
2023-07-07 17:42:58 +0300

    Mandatory tasks are complete

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = parseInteger(argv[1]);
+	b = parseInteger(argv[2]);
+	printf("%d\n", a * b);
+	return (0);
+}
