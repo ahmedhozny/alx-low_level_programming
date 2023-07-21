612f657181a3b79d9b2687246930cdeb62feaa50
PhotonCodes <ahmhosny2002@gmail.com>
2023-07-21 15:00:31 +0300

    Added extra condition to check if format is NULL

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 	i = 0;
-	while (format[i] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
