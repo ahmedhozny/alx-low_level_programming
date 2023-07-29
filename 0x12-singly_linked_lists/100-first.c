28eec86d323557a1d43506439e283833b18d4d3e
PhotonCodes <ahmhosny2002@gmail.com>
2023-07-29 04:31:24 +0300

    Betty things

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+/**
+ * before_main -prints a message before main is called
+ */
 void __attribute__((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		   "I bore my house upon my back!\n");
 }
