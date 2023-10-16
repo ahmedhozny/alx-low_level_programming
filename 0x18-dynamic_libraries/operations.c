3c231dbb97d407f12cfacffef8cbd1131608cd0f
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-10-16 15:20:57 +0300

    Operations functions are now added to a dynamic library

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,56 @@
+#include "operations.h"
+
+/**
+* add - adds two integers
+* @a: first integer
+* @b: second integer
+* Return: the sum
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtracts two integers
+* @a: first integer
+* @b: second integer
+* Return: the difference
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplies two integers
+* @a: first integer
+* @b: second integer
+* Return: the product
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divides two integers
+* @a: first integer
+* @b: second integer
+* Return: the dividend
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  mod - finds the modulus of two integers
+  @a: first integer
+  @b: second integer
+  Return: the remainder of division
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+int add(int i, int k);
+int sub(int i, int k);
+int mul(int i, int k);
+int div(int i, int k);
+int mod(int i, int k);
+
+#endif
