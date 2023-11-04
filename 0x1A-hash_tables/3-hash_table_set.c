5c7dd8318169a52bbe1b4465f3f1755278e9a2bb
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-11-04 03:04:42 +0200

    Hot fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *) key, ht->size);
 	cur = ht->array[index];
 
-	while (cur != NULL && cur->next != NULL)
+	while (cur != NULL)
 	{
 		if (!strcmp(cur->key, key))
 		{
